12ec7a2987d0bea00f1972b8f16cd24291d05391
MalcolmScruggs <mookie.dev@gmail.com>
2019-10-07 12:32:26 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import {SketchField, Tools} from 'react-sketch';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+    sketchContainer: {
+        border: '2px dashed rgb(102, 102, 102)',
+    },
+    toolsContainer: {
+        border: '2px solid rgb(102, 102, 102)',
+        marginTop: '10px'
+    },
+    mainContainer: {
+        margin: '10px',
+    }
+}
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <div className="App">
+                <p>This is the sketch field</p>
+                <div style={styles.mainContainer}>
+                    <div style={styles.sketchContainer}>
+                        <SketchField height='600px'
+                                     tool={Tools.Rectangle}
+                                     lineColor='black'
+                                     lineWidth={3}/>
+                    </div>
+                    <div style={styles.toolsContainer}>
+                        Tools go here
+                        <div onClick={}></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
